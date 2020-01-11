31940ce38ce5778e634005e4706c8b92b340df0e
Alvinosaur <ashek@andrew.cmu.edu>
2020-01-11 02:04:22 -0500

    refactoring

---
+++ object_detection/include/cluster_extraction.h
@@ -0,0 +1,15 @@
+#include <pcl/ModelCoefficients.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+
+typedef pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_ptr;
+
+void find_clusters(pcl_ptr & cloud_filtered, std::vector<pcl::PointIndices> & cluster_groupings);
